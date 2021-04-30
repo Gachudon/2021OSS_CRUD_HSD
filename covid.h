ed4dcf4582eb7fbc6704afab62c9146403c6a11f
KJH <jjihooo49@gmail.com>
2021-04-30 21:59:15 +0900

    file IO, 검색기능 추가

--- covid.h
+++ covid.h
@@ -12,3 +12,9 @@ void readCovid(covid*);         // 데이터 조회
 int selectDataNo(covid**,int);    // 데이터 선택
 int updateCovid(covid**, int);        // 데이터 수정
 int deleteCovid(covid**, int);        // 데이터 삭제
+void searchCovidByName(covid**, int);     //지역명으로 검색
+void searchCovidByConfirm(covid**, int);   //확진자 수로 검색
+void searchCovidByDate(covid**, int);     //마지막 확진자 날짜로 검색
+void searchCovidByHealer(covid**, int);    //완치자 수로 검색
+int loadData(covid**);              //데이터 불러오기
+void saveData(covid**, int);        //데이터 저장하기         
