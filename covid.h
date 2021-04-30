05aab884b134358ee760e48d60bc66b0d85e31c9
HurSeungDon <zzaa9999@naver.com>
2021-04-30 19:05:32 +0900

    covid.h 추가

---
+++ covid.h
@@ -0,0 +1,11 @@
+typedef struct{
+    char name[20];  // 지역이름
+    int confirm;    // 확진자 수
+    int date;       // 마지막 확진날짜 // format: yyyymmdd
+    int healer;     // 완치자 수
+} covid;
+
+int addCovid(covid*);       //데이터 추가
+void readCovid(covid*);     //데이터 조회
+int updateCovid(covid*);    //데이터 수정
+int deleteCovid(covid*);    //데이터 삭제
